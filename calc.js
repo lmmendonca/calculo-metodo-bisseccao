596573c894fd770d42c530cdcc28e88933a1a4de
Laboratório Informática <labinfo@unifil.br>
2019-08-26 20:50:12 -0300

    implemtenta get sinal

--- calc.js
+++ calc.js
@@ -1,17 +1,34 @@
-function input(x5, x4, x3, x2 ,x1, i){
-    x1 = parseFloat(x1.replace("x", i) + "");
-    x2 = parseFloat(x2.replace("x", i) + "");
-    x3 = parseFloat(x3.replace("x", i) + "");
-    x4 = parseFloat(x4.replace("x", i) + "");
-    x5 = parseFloat(x5.replace("x", i) + "");
+function resolver_equacao(x5, x4, x3, x2, x1, i) {
+    x1 = parseFloat(x1 * Math.pow(i, 1));
+    x2 = parseFloat(x2 * Math.pow(i, 2));
+    x3 = parseFloat(x3 * Math.pow(i, 3));
+    x4 = parseFloat(x4 * Math.pow(i, 4));
+    x5 = parseFloat(x5 * Math.pow(i, 5));
     return parseFloat(x5 + x4 + x3 + x2 + x1);
 }
 
-function teste (x5 , x4 , x3 , x2 , x1){
-    var listresult = [];
-    for (i = -100; i <= 100; i++ ){
-        listresult.push(input(x5 , x4 , x3 , x2 , x1, i));
+function gera_intervalo(listresult) {
+    listaIntervalo = [];
+    sinalResultComparator = Math.sign(listresult[0]);
+    for (i = 1; i <= listresult.length(); i++) {
+        sinalResult = Math.abs(listresult[i]);
+        if (sinalResultComparator != sinalResult) {
+            listaIntervalo.push([sinalResultComparator, sinalResult]);
+            sinalResultComparator =  sinalResult;
+        }
     }
+    return listaIntervalo;
+    
+}
+
+function main(x5, x4, x3, x2, x1) {
+    var listresult = [];
+    for (i = -5; i <= 5; i++) {
+        listresult.push(resolver_equacao(x5, x4, x3, x2, x1, i));
+    }
+    listaIntervalo = gera_intervalo(listresult);
     return listresult;
-} 
-teste("-3.72*x", "+5.321*x", "-10.91*x", "-5.2*x", "-7*x");
+}
+
+
+main("-3.72", "+5.321", "-10.91", "-5.2", "-7");
